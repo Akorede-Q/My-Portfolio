6c3d17bbfbc198e2d54eaade94c089d293c07ecb
Akorede Waheed Qoyum <waheedakorede0@gmail.com>
2025-12-04 16:37:48 +0100

    Fix import paths for components
    Updated import paths to use capital 'C' in 'Components'.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Hero from '../components/Hero.jsx';
-import About from '../components/About.jsx';
-import Projects from '../components/Projects.jsx';
-import Skills from '../components/Skills.jsx';
-import Contact from '../components/Contact.jsx';
-import Footer from '../components/Footer.jsx';
+import Hero from '../Components/Hero.jsx';
+import About from '../Components/About.jsx';
+import Projects from '../Components/Projects.jsx';
+import Skills from '../Components/Skills.jsx';
+import Contact from '../Components/Contact.jsx';
+import Footer from '../Components/Footer.jsx';
 
 export default function Home() {
   return (
