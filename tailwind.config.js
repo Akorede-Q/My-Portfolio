369a22e97cda469bbd59b622450c93af93456191
Akorede Waheed Qoyum <waheedakorede0@gmail.com>
2025-12-04 15:07:03 +0100

    Configure Tailwind CSS with content and theme settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,15 @@
-module.exports = {}
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0ea5a4',
+        accent: '#f97316'
+      }
+    }
+  },
+  plugins: []
+};
