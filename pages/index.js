dd102b6f3175b1c1ecfa363256ef84d954bbfd17
Akorede Waheed Qoyum <waheedakorede0@gmail.com>
2025-12-04 16:08:01 +0100

    Change import file extensions from .js to .jsx

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Hero from '../components/Hero';
-import About from '../components/About';
-import Projects from '../components/Projects';
-import Skills from '../components/Skills';
-import Contact from '../components/Contact';
-import Footer from '../components/Footer';
+import Hero from '../components/Hero.jsx';
+import About from '../components/About.jsx';
+import Projects from '../components/Projects.jsx';
+import Skills from '../components/Skills.jsx';
+import Contact from '../components/Contact.jsx';
+import Footer from '../components/Footer.jsx';
 
 export default function Home() {
   return (
